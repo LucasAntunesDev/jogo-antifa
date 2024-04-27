0195528ca44498d72bc2393dfaa0bb7a69128437
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-04-27 10:04:42 -0300

    add: função de montarAlternativas

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const questoes = [
     {
         questão: 1,
         pergunta: 'Qual é a primeira letra do alfabeto?',
+        alternativas: ["a", "b", "c", "d"],
         resposta: 'a',
     },
     {
         questão: 2,
         pergunta: 'Qual é a segunda letra do alfabeto?',
+        alternativas: ["a", "b", "c", "d"],
         resposta: 'c',
     },
     {
         questão: 3,
         pergunta: 'Qual é a terceira letra do alfabeto?',
+        alternativas: ["a", "b", "c", "d"],
         resposta: 'c',
     },
     {
         questão: 4,
         pergunta: 'Qual é a quarta letra do alfabeto?',
+        alternativas: ["a", "b", "c", "d"],
         resposta: 'd',
     }
 ]
@@ -46,3 +50,18 @@ form.addEventListener('submit', (event) => {
     // alert(questoes[0].resposta)
     // alert(document.querySelector(".resposta").value)
 })
+
+const montarAlternativas = (q) => {
+    const lista = document.querySelector('#lista')
+    document.querySelector('#resposta-1-label').innerHTML = q.alternativas[0]
+    document.querySelector('#resposta-2-label').innerHTML = q.alternativas[1]
+    document.querySelector('#resposta-3-label').innerHTML = q.alternativas[2]
+    document.querySelector('#resposta-4-label').innerHTML = q.alternativas[3]
+
+    document.querySelector('#resposta-1').value = q.alternativas[0]
+    document.querySelector('#resposta-2').value = q.alternativas[1]
+    document.querySelector('#resposta-3').value = q.alternativas[2]
+    document.querySelector('#resposta-4').value = q.alternativas[3]
+}
+
+montarAlternativas(questoes[0])
